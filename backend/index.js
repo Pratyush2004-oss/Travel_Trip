cbee62a912d1a6a8555fcfaa091587ed5e8258ef
Pratyush2004-oss <pratyush@sitp.ac.in>
2025-04-14 14:52:05 +0530

    Changing ath

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './lib/dbConn.js';
-import path, { join } from 'path';
 
 // importing routes
 import placesRoute from './routes/Places.route.js';
 import bookingRoute from './routes/booking.routes.js';
 
 dotenv.config();
-const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -23,10 +21,6 @@ app.use((err, req, res, next) => {
     res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal Server Error" : "Internal Server Error" + err.message });
 })
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path, join(__dirname, "./frontend/dist")));
-    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "./frontend/dist/index.html")));
-}
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server running on port ${PORT}`);
